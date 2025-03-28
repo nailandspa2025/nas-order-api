c065f5fe7b7bbdab2940ec0fa08bf57c7faf4b6b
“canhvanlam” <canhvanlam@salescore.me>
2025-03-28 14:45:30 +0700

    fix

---
+++ BuildingBlocks/Common/Logging/LoggerFactoryExtensions.cs
@@ -0,0 +1,82 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Amazon.CloudWatchLogs;
+using Serilog;
+using Serilog.Sinks.AwsCloudWatch;
+using Serilog.Sinks.GoogleCloudLogging;
+
+namespace BuildingBlocks.Common.Logging;
+
+public static class LoggerFactoryExtensions
+{
+    const string MessageTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level}] {Message} LogProperties={Properties}{NewLine}{Exception}";
+
+    public static void AddGoogleCloudSerilog(this ILoggerFactory loggerFactory, IConfiguration configuration, GoogleCloudLoggingSinkOptions sinkOptions = null)
+    {
+        var loggerBuilder = AddCommonLoggerConfiguration(configuration);
+
+        if (sinkOptions == null)
+        {
+            string cloudRoleName = Assembly.GetEntryAssembly().GetName().Name;
+            sinkOptions = new GoogleCloudLoggingSinkOptions
+            {
+                LogName = cloudRoleName,
+                ServiceName = cloudRoleName,
+                UseLogCorrelation = true,
+                UseSourceContextAsLogName = false,
+            };
+        }
+
+        loggerBuilder
+           .WriteTo.GoogleCloudLogging(sinkOptions);
+
+        Log.Logger = loggerBuilder.CreateLogger();
+        loggerFactory.AddSerilog();
+    }
+
+    public static void AddAmazonCloudSerilog(this ILoggerFactory loggerFactory, IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
+    {
+        var loggerBuilder = AddCommonLoggerConfiguration(configuration);
+        var client = new AmazonCloudWatchLogsClient();
+        string cloudRoleName = Assembly.GetEntryAssembly().GetName().Name;
+
+        loggerBuilder
+             .WriteTo.AmazonCloudWatch(
+                // The name of the log group to log to
+                logGroup: $"{cloudRoleName}_{webHostEnvironment.EnvironmentName}",
+                // A string that our log stream names should be prefixed with. We are just specifying the
+                // start timestamp as the log stream prefix
+                logStreamPrefix: DateTime.UtcNow.ToString("yyyyMMddHHmmssfff"),
+                // The AWS CloudWatch client to use
+                cloudWatchClient: client);
+
+        Log.Logger = loggerBuilder.CreateLogger();
+        loggerFactory.AddSerilog();
+    }
+
+    public static void AddFileSerilog(this ILoggerFactory loggerFactory, IConfiguration configuration, string contentRootPath)
+    {
+        var loggerBuilder = AddCommonLoggerConfiguration(configuration);
+
+        loggerBuilder
+            .WriteTo.File(path: Path.Combine($"{contentRootPath}\\logs", "log-.txt"),
+            rollingInterval: RollingInterval.Day,
+            outputTemplate: MessageTemplate)
+            .WriteTo.Console(outputTemplate: MessageTemplate);
+
+        Log.Logger = loggerBuilder.CreateLogger();
+        loggerFactory.AddSerilog();
+    }
+
+    private static LoggerConfiguration AddCommonLoggerConfiguration(IConfiguration configuration)
+        => new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .Enrich.FromLogContext()
+                
+                .Filter.ByExcluding(c => c.Properties.Any(p => p.Value.ToString().Contains("/liveness")))
+                .Filter.ByExcluding(c => c.Properties.Any(p => p.Value.ToString().Contains("/hc")))
+                .Filter.ByExcluding(c => c.Properties.Any(p => p.Value.ToString().Contains("/swagger")))
+                .Filter.ByExcluding(c => c.Properties.Any(p => p.Value.ToString().Contains("/health")));
+}
---
+++ BuildingBlocks/Common/Mediator/Behaviours/LoggingBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using BuildingBlocks.Authentication.Abstractions;
+using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+
+namespace BuildingBlocks.Common.Mediator.Behaviours;
+
+public class LoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest> where TRequest : notnull
+{
+    private readonly ILogger _logger;
+    private readonly ICurrentUser _currentUser;
+
+    public LoggingBehaviour(ILogger<TRequest> logger, ICurrentUser currentUser)
+    {
+        _logger = logger;
+        _currentUser = currentUser;
+    }
+
+    public async Task Process(TRequest request, CancellationToken cancellationToken)
+    {
+        var requestName = typeof(TRequest).Name;
+        var userId = _currentUser.UserId;
+        var userName = _currentUser.UserName;
+
+        const string prefix = nameof(LoggingBehaviour<TRequest>);
+
+        _logger.LogInformation("[{Prefix}] Request: {Name} {@UserId} {@UserName} {@Request}",
+            prefix, requestName, userId, userName, request);
+
+        await Task.CompletedTask;
+    }
+}
+
