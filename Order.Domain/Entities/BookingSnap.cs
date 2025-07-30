c9173bcc8d29c796c153bab5f04882ec362949c0
canhvanlam <canhvanlam@gmail.com>
2025-07-30 21:22:04 +0700

    fix booking

--- Order.Domain/Entities/BookingSnap.cs
+++ Order.Domain/Entities/BookingSnap.cs
@@ -4,7 +4,6 @@ namespace Order.Domain.Entities;
 
 public class BookingSnap : BaseAuditableEntity<int>
 {
-    public long BookingId { get; set; }
     public string SnapId { get; set; } = null!;
     public Booking Booking { get; set; } = null!;
 }
--- Order.Domain/Entities/BookingSnapGroup.cs
+++ Order.Domain/Entities/BookingSnapGroup.cs
@@ -4,7 +4,6 @@ namespace Order.Domain.Entities;
 
 public class BookingSnapGroup : BaseAuditableEntity<int>
 {
-    public long BookingId { get; set; }
     public string GroupdId { get; set; } = null!;
     public Booking Booking { get; set; } = null!;
 }
