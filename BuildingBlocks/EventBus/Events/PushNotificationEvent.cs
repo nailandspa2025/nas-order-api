09dff63c7bf56921d0fa61847ccffe8117c1bf1f
canhvanlam <canhvanlam@gmail.com>
2026-02-04 01:04:59 +0700

    fix notification

--- BuildingBlocks/EventBus/Events/PushNotificationEvent.cs
+++ BuildingBlocks/EventBus/Events/PushNotificationEvent.cs
@@ -3,5 +3,6 @@ namespace BuildingBlocks.EventBus.Events;
 public class PushNotificationEvent
 {
     public string UserId { get; set; }
-    public string Content { get; set;}
+    public string Content { get; set; }
+    public List<string> Tokens { get; set; } = new List<string>();
 }
--- Order.Application/Features/Notifications/Commands/PushNotification/PushNotificationMessageEvent.cs
+++ Order.Application/Features/Notifications/Commands/PushNotification/PushNotificationMessageEvent.cs
@@ -1,4 +1,3 @@
-using BuildingBlocks.ApiClients.Clients.Identity;
 using BuildingBlocks.Common.Firebase;
 using FirebaseAdmin.Messaging;
 using MediatR;
@@ -11,35 +10,33 @@ public record PushNotificationMessageEvent : IRequest<Unit>
 {
     public string UserId { get; init; }
     public string Content { get; init; }
+    public List<string> Tokens { get; init; } = new List<string>();
 }
 
 public class PushNotificationMessageEventHandler : IRequestHandler<PushNotificationMessageEvent, Unit>
 {
     private readonly IOrderDbContext _context;
-    private readonly IIdentityClient _identityClient;
     private readonly IFirebaseService _firebaseService;
     private readonly ILogger<PushNotificationMessageEventHandler> _logger;
 
-    public PushNotificationMessageEventHandler(IOrderDbContext context, IIdentityClient identityClient, IFirebaseService firebaseService, ILogger<PushNotificationMessageEventHandler> logger)
+    public PushNotificationMessageEventHandler(IOrderDbContext context, IFirebaseService firebaseService, ILogger<PushNotificationMessageEventHandler> logger)
     {
         _context = context;
-        _identityClient = identityClient;
         _firebaseService = firebaseService;
         _logger = logger;
 
     }
     public async Task<Unit> Handle(PushNotificationMessageEvent request, CancellationToken cancellationToken)
     {
-            var deviceTokens = new List<string>
-            {
-                "ckkWLzCV4k3Omu9V_qFhLr:APA91bHayeD4zCBY4bRIJDqFnIPhHpS5U76hbVhR4GsM-s8mv8a_m5AalFjCe3zD-0c9OF9eNlrUbearVbh1zJmVlQHw17Nn9KEdIS2gFbhapWzjgpXWA0Q",
-                "cSlkVc9VziTcx66JjdnXly:APA91bF-LxvwSUMtxjTF9O4fU1edgVVMgXdTWilZpgrx_Z37GStb9w8cVM6aSzuonsXvKVFHFGAjoNjwsDL52EsR927g9tqMimuOM0QowCB9WH-ciH4G9NI",
-                "dR2PWyXUSYaZUVzsIy8iSe:APA91bE_nGHZHZqX2t7Wci1878gTmyOEDMYHEA77kxEPNsP0nQnQ8skX8b4c8RP9Y63r9Iy33p6QTT0Sh2hJHsiD8U3BQLHzq1x9ulQ6CcIh6AC4EFfPxec"
-            };
-        
+            // var deviceTokens = new List<string>
+            // {
+            //     "ckkWLzCV4k3Omu9V_qFhLr:APA91bHayeD4zCBY4bRIJDqFnIPhHpS5U76hbVhR4GsM-s8mv8a_m5AalFjCe3zD-0c9OF9eNlrUbearVbh1zJmVlQHw17Nn9KEdIS2gFbhapWzjgpXWA0Q",
+            //     "cSlkVc9VziTcx66JjdnXly:APA91bF-LxvwSUMtxjTF9O4fU1edgVVMgXdTWilZpgrx_Z37GStb9w8cVM6aSzuonsXvKVFHFGAjoNjwsDL52EsR927g9tqMimuOM0QowCB9WH-ciH4G9NI",
+            //     "dR2PWyXUSYaZUVzsIy8iSe:APA91bE_nGHZHZqX2t7Wci1878gTmyOEDMYHEA77kxEPNsP0nQnQ8skX8b4c8RP9Y63r9Iy33p6QTT0Sh2hJHsiD8U3BQLHzq1x9ulQ6CcIh6AC4EFfPxec"
+            // };
             var message = new MulticastMessage
             {
-                Tokens = deviceTokens,
+                Tokens = request.Tokens,
                 Notification = new FirebaseAdmin.Messaging.Notification
                 {
                     Title = "New message",
