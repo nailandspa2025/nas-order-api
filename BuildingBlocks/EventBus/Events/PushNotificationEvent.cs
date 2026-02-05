92da4399f0336864ca31289d39cfdd89deebb144
canhvanlam <canhvanlam@gmail.com>
2026-02-05 17:39:57 +0700

    Update PushNotificationEvent.cs

--- BuildingBlocks/EventBus/Events/PushNotificationEvent.cs
+++ BuildingBlocks/EventBus/Events/PushNotificationEvent.cs
@@ -5,5 +5,6 @@ public class PushNotificationEvent
     public string UserId { get; set; }
     public string Content { get; set; }
     public List<string> Tokens { get; set; } = new List<string>();
-    public string senderInfo { get; set; }
+    public string SenderInfo { get; set; }
+    public string ReceiverInfo { get; set; }
 }
