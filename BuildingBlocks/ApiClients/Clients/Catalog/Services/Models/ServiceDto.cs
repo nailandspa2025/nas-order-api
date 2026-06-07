00b17e0b4a2b769dbec4b81bb0991a0494287252
canhvanlam <canhvanlam@gmail.com>
2026-06-08 00:49:07 +0700

    Update ServiceDto.cs

--- BuildingBlocks/ApiClients/Clients/Catalog/Services/Models/ServiceDto.cs
+++ BuildingBlocks/ApiClients/Clients/Catalog/Services/Models/ServiceDto.cs
@@ -5,5 +5,7 @@ public class ServiceDto
     public int Id { get; set; }
     public string? Name { get; set; }
     public string? Code { get; set; }
-    public TimeSpan ? WorkingTime { get; set; }
+    public TimeSpan? WorkingTime { get; set; }
+    public string ? UrlImage { get; set; }
+
 }
