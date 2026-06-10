0429554240e6da9eaeac623ccb42b4a6bf1a8bb0
canhvanlam <canhvanlam@gmail.com>
2026-06-10 14:42:41 +0700

    price

--- BuildingBlocks/ApiClients/Clients/Catalog/Services/Models/ServiceDto.cs
+++ BuildingBlocks/ApiClients/Clients/Catalog/Services/Models/ServiceDto.cs
@@ -6,6 +6,9 @@ public class ServiceDto
     public string? Name { get; set; }
     public string? Code { get; set; }
     public TimeSpan? WorkingTime { get; set; }
-    public string ? UrlImage { get; set; }
+    public string? UrlImage { get; set; }
+    public decimal PriceTo { get; set; }
+
+    public decimal PriceFrom { get; set; }
 
 }
